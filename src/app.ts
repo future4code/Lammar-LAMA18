4e1fffcfed8c4a4ad20990bbaa4191aa3de9f9bd
gans92 <gabri.alisson.23@gmail.com>
2023-03-14 00:11:44 -0500

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { AddressInfo } from "net";
+
+export const app = express();
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cors())
+
+const server = app.listen(process.env.PORT ||3000, () => {
+    if (server) {
+      const address = server.address() as AddressInfo;
+      console.log(`🚀 Servidor rodando em http://localhost:${address.port}`);
+    } else {
+      console.error(`Falha ao rodar o servidor.`);
+    }
+  });
